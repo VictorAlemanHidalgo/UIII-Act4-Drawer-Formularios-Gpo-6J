ca02be2e645b91c315e992932a365d20545fd84b
a.22308051281123 <a.22308051281123@cbtis128.edu.mx>
2025-06-07 01:09:09 +0000

    x

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           InkWell(
             onTap: null,
-            child: new IconButton(
+            child: IconButton(
               icon: Icon(
                 Icons.search,
                 color: Colors.white,
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
           ),
           InkWell(
             onTap: null,
-            child: new IconButton(
+            child: IconButton(
               icon: Icon(
                 Icons.settings,
                 color: Colors.white,
